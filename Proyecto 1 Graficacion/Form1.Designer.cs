3a3000d709b22073785a06ba217c3214e2e0d920
ZauAlpha <zaurielec@gmail.com>
2023-02-08 06:27:42 -0600

    Ahora si el final JAJAJA

--- Proyecto 1 Graficacion/Form1.Designer.cs
+++ Proyecto 1 Graficacion/Form1.Designer.cs
@@ -41,9 +41,6 @@
             this.NEW_FRAME = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.Animate = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TB_ROTATION)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TB_SCALE)).BeginInit();
@@ -175,43 +172,11 @@
             this.Animate.UseVisualStyleBackColor = false;
             this.Animate.Click += new System.EventHandler(this.Animate_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(673, 711);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 14;
-            this.label4.Text = "label4";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(49, 666);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(49, 703);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1521, 862);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.Animate);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.NEW_FRAME);
@@ -249,8 +214,5 @@
         private Button NEW_FRAME;
         private Label label2;
         private Button Animate;
-        private Label label4;
-        private Button button1;
-        private Button button2;
     }
 }
